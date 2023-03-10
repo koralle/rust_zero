bb219d8f06b431f8b61d99d63f50da811cd15b31
koralle <mkhd7930@gmail.com>
2023-03-10 21:27:20 +0900

    add: enum AST

--- regex-engine/src/engine/parser.rs
+++ regex-engine/src/engine/parser.rs
@@ -1,1 +1,12 @@
 
+/// 抽象構文木を表現するための型
+#[derive(Debug)]
+pub enum AST {
+    Char(char),
+    Plus(Box<AST>),
+    Star(Box<AST>),
+    Question(Box<AST>),
+    Or(Box<AST>, Box<AST>),
+    Seq(Vec<AST>),
+}
+
